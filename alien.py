03a5d4b2ef5aeb1385af90e3549510e8d85fa341
Lasantha Mudalige <lasantha1130@gmail.com>
2022-12-04 12:54:30 +0530

    remove old methods and add a new method

--- alien.py
+++ alien.py
@@ -5,21 +5,14 @@ class Alien(Turtle):
 
     def __init__(self, position):
         super(Alien, self).__init__(shape="square")
-        self.direction = "left"
         self.hideturtle()
         self.color("blue")
         self.shapesize(1, 1)
         self.penup()
         self.goto(position)
 
-    def move_left(self):
+    def move(self):
         self.showturtle()
-        new_x = self.xcor() - 10
-        if new_x >= -410:
-            self.goto(new_x, self.ycor())
-
-    def move_right(self):
-        self.showturtle()
-        new_x = self.xcor() + 10
-        if new_x <= 410:
-            self.goto(new_x, self.ycor())
+        new_y = self.ycor() - 5
+        if new_y <= 410:
+            self.goto(self.xcor(), new_y)
