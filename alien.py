0f0e7b74a81b2db989d827a1123a7e015eb3f38c
lasantha <lasantha1130@gmail.com>
2022-12-26 15:50:43 +0530

    remove check for the edge of the screen

--- alien.py
+++ alien.py
@@ -8,7 +8,6 @@ class Alien(Turtle):
         super(Alien, self).__init__(shape="square")
         self.name = "alien"
         self.direction = "left"
-        self.got_hit = False
         self.hideturtle()
         self.color("white")
         self.shapesize(1, 1)
@@ -18,21 +17,17 @@ class Alien(Turtle):
 
     def move_down(self):
         new_y = self.ycor() - 40
-        if new_y <= 410:
-            self.goto(self.xcor(), new_y)
+        self.goto(self.xcor(), new_y)
 
     def move_left(self):
         new_x = self.xcor() - 40
-        if new_x >= -400:
-            self.goto(new_x, self.ycor())
+        self.goto(new_x, self.ycor())
 
     def move_right(self):
         new_x = self.xcor() + 40
-        if new_x <= 400:
-            self.goto(new_x, self.ycor())
+        self.goto(new_x, self.ycor())
 
     def hide(self):
-        # self.got_hit = True
         self.hideturtle()
 
     def get_new_bullet(self):
