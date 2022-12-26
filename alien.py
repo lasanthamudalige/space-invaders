6080776a8998ee1ca50310d3e3a324d80b97a591
lasantha <lasantha1130@gmail.com>
2022-12-26 15:55:09 +0530

    add comments

--- alien.py
+++ alien.py
@@ -16,20 +16,24 @@ class Alien(Turtle):
         self.showturtle()
 
     def move_down(self):
+        # Reduce 40 from the current y position of the alien.
         new_y = self.ycor() - 40
         self.goto(self.xcor(), new_y)
 
     def move_left(self):
+        # Reduce 40 from the current x position of the alien.
         new_x = self.xcor() - 40
         self.goto(new_x, self.ycor())
 
     def move_right(self):
+        # Add 40 to current x position of the alien.
         new_x = self.xcor() + 40
         self.goto(new_x, self.ycor())
 
     def hide(self):
         self.hideturtle()
 
+    # Get a new bullet from the alien's position.
     def get_new_bullet(self):
         new_bullet = Bullet(
             shape="circle", position=(self.xcor(), self.ycor()))
