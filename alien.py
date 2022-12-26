59d1e9d9da32e757e5169a65ab536edf6f04fea4
lasantha <lasantha1130@gmail.com>
2022-12-26 11:40:34 +0530

    rename fuctions

--- alien.py
+++ alien.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+from bullet import Bullet
 
 
 class Alien(Turtle):
@@ -6,6 +7,7 @@ class Alien(Turtle):
     def __init__(self, position):
         super(Alien, self).__init__(shape="square")
         self.name = "alien"
+        self.direction = "left"
         self.got_hit = False
         self.hideturtle()
         self.color("white")
@@ -13,12 +15,27 @@ class Alien(Turtle):
         self.penup()
         self.goto(position)
 
-    def move(self):
+    def move_down(self):
         self.showturtle()
-        new_y = self.ycor() - 5
+        new_y = self.ycor() - 40
         if new_y <= 410:
             self.goto(self.xcor(), new_y)
+
+    def move_left(self):
+        new_x = self.xcor() - 40
+        if new_x >= -400:
+            self.goto(new_x, self.ycor())
+
+    def move_right(self):
+        new_x = self.xcor() + 40
+        if new_x <= 400:
+            self.goto(new_x, self.ycor())
 
     def hide(self):
         self.got_hit = True
         self.hideturtle()
+
+    def get_new_bullet(self):
+        new_bullet = Bullet(
+            shape="circle", position=(self.xcor(), self.ycor()))
+        return new_bullet
