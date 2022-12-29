36648776c52160efb4dc277eead75783217fdc05
lasantha <lasantha1130@gmail.com>
2022-12-29 16:53:29 +0530

    add hide function

--- barrier.py
+++ barrier.py
@@ -4,5 +4,9 @@ class Barrier(Turtle):
 
     def __init__(self, shape , position):
         super(Barrier, self).__init__(shape=shape)
+        self.health = 100
         self.penup()
         self.goto(position)
+
+    def hide(self):
+        self.hideturtle()
